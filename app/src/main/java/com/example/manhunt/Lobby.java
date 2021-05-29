bb06ea3185de20eb2d74aed76a7c75151168f688
julianLapenna12 <7jlapenna@gmail.com>
2021-05-29 19:42:15 -0400

    Added functionality to username saving (lobby page) and display on list of players, title page has logo, rearranged lobby wait-room view.

--- app/src/main/java/com/example/manhunt/Lobby.java
+++ app/src/main/java/com/example/manhunt/Lobby.java
@@ -6,16 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.security.cert.PKIXRevocationChecker;
 
 public class Lobby extends AppCompatActivity {
 
+    String name;
+
+    EditText nameInput;
+    Button saveName;
+
+    TextView lobbyView = null;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        saveName = (Button) findViewById(R.id.btnName);
+        lobbyView = (TextView) findViewById(R.id.lobbyView);
+
+        lobbyView.setText(null);
+
+        saveName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = nameInput.getText().toString();
+                lobbyView.append(name + "\n");
+
+                showToast(name);
+            }
+        });
 
         final ImageButton button = findViewById(R.id.settings);
 
@@ -27,5 +55,9 @@ public class Lobby extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, "Name updated!", Toast.LENGTH_SHORT).show();
     }
 }
