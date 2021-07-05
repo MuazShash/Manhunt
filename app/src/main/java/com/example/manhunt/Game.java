b9d06dd6406b5b3377cc1bdd58b1eb6f5e8f56ad
Muaz Shash <muaz.shash@gmail.com>
2021-07-05 16:36:59 -0400

    sound fix v2

--- app/src/main/java/com/example/manhunt/Game.java
+++ app/src/main/java/com/example/manhunt/Game.java
@@ -539,7 +539,7 @@ public class Game extends FragmentActivity implements OnMapReadyCallback {
                 else if(distanceInMeters > 30 || distanceInMeters < globalPlayer.getSettings(2) && mpApproaching.isPlaying()){
                     mpApproaching.stop();
                 }
-                else{
+                else if (distanceInMeters < 30 && distanceInMeters > globalPlayer.getSettings(2) && mpApproaching.isPlaying()){
                     am.setStreamVolume(AudioManager.STREAM_MUSIC, (int) Math.ceil(am.getStreamMaxVolume(AudioManager.STREAM_MUSIC)*1/(distanceInMeters+1)), 0);
                 }
             }
