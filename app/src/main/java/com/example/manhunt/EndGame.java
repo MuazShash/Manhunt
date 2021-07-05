9ad6b46224cae5a1b992c8417eca0fba9a150f1c
Omar Bafagih <bafagihomar260@gmail.com>
2021-07-05 01:36:08 -0400

    Added functionality to end game screen (will return to start activity).
    Completed design of the end game screen.

--- app/src/main/java/com/example/manhunt/EndGame.java
+++ app/src/main/java/com/example/manhunt/EndGame.java
@@ -1,8 +1,10 @@
 package com.example.manhunt;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class EndGame extends AppCompatActivity {
@@ -11,9 +13,19 @@ public class EndGame extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game);
+
+
+        final Button backToStart = (Button) findViewById(R.id.backToStart);
+
+        backToStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EndGame.this, CreateGamePopup.class));
+            }
+        });
+
 
     }
-
-
 
 }
