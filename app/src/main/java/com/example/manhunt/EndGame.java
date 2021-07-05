dca9051289b680a3e0d978e3066946ed9529a44f
Omar Bafagih <bafagihomar260@gmail.com>
2021-07-05 00:05:17 -0400

    Actually Added EndGame java class and xml view.

---
+++ app/src/main/java/com/example/manhunt/EndGame.java
@@ -0,0 +1,19 @@
+package com.example.manhunt;
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+
+
+public class EndGame extends AppCompatActivity {
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+}
